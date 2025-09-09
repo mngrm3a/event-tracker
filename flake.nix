d67e73ae9237eecb8e393d2ba3763bad758b846d
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2025-09-09 16:03:02 +0200

    Initial vite setup

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "die-bible.de website scraping and processing";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs
+            ];
+          };
+        }
+      );
+    };
+}
