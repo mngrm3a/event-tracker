1a1016aad064e9c376d1422e2e964bae16cc61f9
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2025-09-12 20:52:26 +0200

    Add AppWithContext in order to use contexts

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { StackLayout } from '@/components/StackLayout';
 import { CurrentDateProvider } from '@/providers/CurrentDateProvider';
 import { StoreProvider } from '@/providers/StoreProvider';
 
-function App() {
-  return (
-    <CurrentDateProvider>
-      <StoreProvider>
-        <StackLayout>
-          <></>
-          <></>
-          <SliderButton delay={1500} />
-        </StackLayout>
-      </StoreProvider>
-    </CurrentDateProvider>
-  );
-}
-
+const App = () => (
+  <CurrentDateProvider>
+    <StoreProvider>
+      <AppWithContext />
+    </StoreProvider>
+  </CurrentDateProvider>
+);
 export default App;
+
+const AppWithContext = () => {
+  return (
+    <StackLayout>
+      <></>
+      <></>
+      <SliderButton delay={1500} />
+    </StackLayout>
+  );
+};
