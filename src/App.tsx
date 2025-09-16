2db05d2bbe7440ca0c011ad0913b28be16d1c4d7
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2025-09-16 17:21:59 +0200

    Add week, month and year chart

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,26 @@ import { TodayProvider } from '@/providers/TodayProvider';
 import { StoreProvider } from '@/providers/StoreProvider';
 import { TodayChart } from '@/components/TodayChart';
 import { LoadingWrapper } from '@/components/LoadingWrapper';
-import { ArcElement, Chart, RadialLinearScale } from 'chart.js';
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart,
+  LinearScale,
+  RadialLinearScale,
+} from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Carousel, CarouselSlide } from '@/components/Carousel';
+import { BarChart } from '@/components/BarChart';
 
-Chart.register(RadialLinearScale, ArcElement, ChartDataLabels);
-
+Chart.register(
+  RadialLinearScale,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ChartDataLabels,
+);
 const App = () => (
   <TodayProvider resolution={60000}>
     <StoreProvider>
@@ -37,7 +52,41 @@ const AppWithContext = () => {
         countsByPeriod={countsByPeriod}
       >
         <StackLayout>
-          <></>
+          <Carousel>
+            <CarouselSlide>
+              <BarChart
+                data={countsByPeriod.weekData}
+                labels={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']}
+              />
+            </CarouselSlide>
+            <CarouselSlide>
+              <BarChart
+                data={countsByPeriod.monthData}
+                labels={Array(31)
+                  .fill(0)
+                  .map((_, i) => String(i + 1))}
+              />
+            </CarouselSlide>
+            <CarouselSlide>
+              <BarChart
+                data={countsByPeriod.yearData}
+                labels={[
+                  'Jan',
+                  'Feb',
+                  'Mar',
+                  'Apr',
+                  'May',
+                  'Jun',
+                  'Jul',
+                  'Aug',
+                  'Sep',
+                  'Oct',
+                  'Nov',
+                  'Dec',
+                ]}
+              />
+            </CarouselSlide>
+          </Carousel>
           <TodayChart data={countsByPeriod.hourData} />
           <SliderButton
             counter={countsByPeriod.todayData}
