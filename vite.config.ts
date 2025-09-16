4d60081906aca5d74aab5b85eaa852a178d3e6dd
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2025-09-16 18:08:03 +0200

    Enable PWA features via the vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { themeExtractorPlugin } from './vite-plugins/vite-plugin-theme';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,32 @@ export default defineConfig({
       cssPath: 'src/index.css',
       outFile: 'src/themeColors.ts',
       resolve: true,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+      },
+      manifest: {
+        name: 'Event Tracker',
+        short_name: 'Event Tracker',
+        theme_color: '#8ec5ff',
+        icons: [
+          {
+            src: 'logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
   ],
   base: '/event-tracker/',
